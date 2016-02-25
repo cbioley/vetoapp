bc03e0e4004a558682380462ea2daf73be039eea
Daniel Steigerwald <daniel@steigerwald.cz>
2016-02-25 01:01:01 +0100

    Fix build

--- src/browser/home/Page.react.js
+++ src/browser/home/Page.react.js
@@ -2,7 +2,7 @@ import Component from 'react-pure-render/component';
 import Helmet from 'react-helmet';
 import React, { PropTypes } from 'react';
 import { FormattedHTMLMessage } from 'react-intl';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 class Page extends Component {
