2355b40600c7a57069ae490f3a2f41fc553404b7
Daniel Steigerwald <daniel@steigerwald.cz>
2016-03-26 23:11:42 +0100

    Make verification visible

--- src/server/frontend/index.js
+++ src/server/frontend/index.js
@@ -14,6 +14,7 @@ app.use(compression());
 // All assets must be handled via require syntax like this:
 // <img alt="50x50 placeholder" src={require('./50x50.png')} />
 app.use('/assets', express.static('build', { maxAge: '200d' }));
+app.use('/google56574e809399bcf2.html', express.static('google56574e809399bcf2.html'));
 
 app.get('*', render);
 
